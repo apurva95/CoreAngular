56786f1ca9ae37a507579aa62d00937a4014abb1
HeyKshitij <ksharma860@gmail.com>
2021-07-18 01:40:41 -0700

    Adding Users controller and updating api to use it

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'Dating App';
+  Users: any;
+  /**
+   * Dependency inject for the httpclient
+   */
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {
+   
+    this.http.get("https://localhost:44396/api/User").subscribe()
+  }
+  
 }
